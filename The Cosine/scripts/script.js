f743e6bcee1daabea63203082ef0d47292f1536e
sofixyang <90701749+sofixyang@users.noreply.github.com>
2021-09-24 09:43:16 +0200

    Add files via upload

--- The Cosine/scripts/script.js
+++ The Cosine/scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
